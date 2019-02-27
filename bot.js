60bb92231d1e3781807af21d3383d582e6f5f4fa
Mehdi7894 <47418152+Mehdi7894@users.noreply.github.com>
2019-02-27 15:41:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,9 +7,13 @@ client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
 **
-سلام عليكم 
-لو سمحت ممكن تدخل سيرفرى
-                               [ https://discord.gg/KmR4Vr9 ] 
+هلا وغلا قدمت اهلاً ووطأت سهلاً نرحب بك بباقة
+زهور يسعدني أن أرحب بك أجمل ترحيب وأشكرك على إنضمامك معنا
+بين أخواتك وإخوانك متمنياً لك طيب الأقامة مع المتعة
+والفائدة وننتظر منك الجديد والمفيد.
+
+
+                               [ https://discord.gg/VeqgsM7 ] 
 الدعووة خاصة لك ... [ ${member}  ]
 **`) 
 }).catch(console.error)
